428a60d2ad20ba0524b7c23d1ce0b9d5124e8fd7
Vishal <78654619+chvishal182@users.noreply.github.com>
2021-07-18 01:26:37 +0530

    Add files via upload

---
+++ TimeTable_S/build/generated/source/buildConfig/debug/com/cvishaldev/timetable_s/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cvishaldev.timetable_s;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.cvishaldev.timetable_s";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
